25a0c82231d9afd9ea2d8da5a2dff94ba83dd075
Adeel984 <43789830+Adeel984@users.noreply.github.com>
2019-05-20 17:42:24 +0500

    New code added for categorized items to display on users screen.

--- app/src/main/java/com/example/i_auction/Fragments/bidder_HomeFragment.kt
+++ app/src/main/java/com/example/i_auction/Fragments/bidder_HomeFragment.kt
@@ -34,34 +34,90 @@ private const val ARG_PARAM2 = "param2"
  */
 class bidder_HomeFragment : Fragment() {
     lateinit var adapter: ItemsRVAdapter
-    var itemList: ArrayList<Items> = ArrayList()
+    var categorisedList = ArrayList<Items>()
+    var itemsList = ArrayList<Items>()
     val userId = FirebaseAuth.getInstance().currentUser?.uid
     var value = 0
     var maxVal = 0
+    var categorisedItems: Boolean = false
     lateinit var wait_dialog: Dialog
-    lateinit var dialog:Dialog
-
+    lateinit var dialog: Dialog
+    lateinit var bidderClicked: (views: View, position: Int) -> Unit
+    lateinit var recyler: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_bidder__home, container, false)
-        val recyler: RecyclerView = view.findViewById(R.id.bidder_main_rv)
+        val categorySpinner = view.findViewById<Spinner>(R.id.bidder_home_spinner)
+
+        ArrayAdapter.createFromResource(activity!!, R.array.category_items_array, android.R.layout.simple_spinner_item)
+            .also {
+                it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                categorySpinner.adapter = it
+            }
+        categorySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val item = parent?.getItemAtPosition(position).toString()
+                if (position > 0) {
+                    categorisedItems = true
+                    showCategorisedItems(item, categorisedItems)
+                } else {
+                    categorisedItems = false
+                    showCategorisedItems(item, categorisedItems)
+                }
+            }
+
+        }
+        recyler = view.findViewById(R.id.bidder_main_rv)
         recyler.layoutManager = LinearLayoutManager(activity!!)
         wait_dialog = Dialog(activity!!)
         dialog = Dialog(activity!!)
-        val bidderClicked: (views: View, position: Int) -> Unit = { views, position ->
+        bidderClicked = { views, position ->
             when (views.id) {
                 R.id.bid_now_view -> showBidDialogue(position)
-                R.id.withdraw_bid_view -> applyOrWithDrawFromBid(true, position, "0")
+                R.id.withdraw_bid_view -> {
+                    when (categorisedItems) {
+                        true -> applyOrWithDrawFromBid(true, position, "0", categorisedList)
+                        false -> applyOrWithDrawFromBid(true, position, "0", itemsList)
+                    }
+                }
             }
         }
-        adapter = ItemsRVAdapter(activity!!, itemList, bidderClicked)
+        adapter = ItemsRVAdapter(activity!!, itemsList, bidderClicked)
         recyler.layoutManager = LinearLayoutManager(activity!!)
         recyler.adapter = adapter
         getAllItems()
         return view
+    }
+
+    private fun showCategorisedItems(names: String, categorised: Boolean) {
+        when (categorised) {
+            true -> {
+                adapter = ItemsRVAdapter(activity!!, categorisedList, bidderClicked)
+                recyler.adapter = adapter
+                categorisedList.clear()
+                itemsList.forEach { item ->
+                    if (item.itemCategory.equals(names)) {
+                        Log.d("Category", names)
+                        categorisedList.add(item)
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+            else -> {
+                adapter = ItemsRVAdapter(activity!!, itemsList, bidderClicked)
+                recyler.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+
     }
 
     private fun showBidDialogue(position: Int) {
@@ -75,7 +131,11 @@ class bidder_HomeFragment : Fragment() {
         val minBidAmount = dialog.findViewById<TextView>(R.id.min_bid_amount_bidderView)
         val maxBidAmount = dialog.findViewById<TextView>(R.id.max_bid_amount_bidderView)
         val applyBid = dialog.findViewById<Button>(R.id.apply_bid)
-        val item = itemList[position]
+        var item = Items()
+        when (categorisedItems) {
+            true -> item = categorisedList[position]
+            else -> item = itemsList[position]
+        }
         value = item.min_bid_amount.toInt()
         try {
             bidValue.setText(value.toString())
@@ -84,9 +144,9 @@ class bidder_HomeFragment : Fragment() {
             maxBidAmount.setText(item.max_bid_amount)
             itemName.text = item.itemName
             item.bidded_users?.values?.forEach {
-            if(it >=maxVal) {
-                maxVal = it
-            }
+                if (it >= maxVal) {
+                    maxVal = it
+                }
             }
             maxBidAmount.setText(maxVal.toString())
         } catch (e: Exception) {
@@ -112,51 +172,60 @@ class bidder_HomeFragment : Fragment() {
             title.setText("Applying Bid")
             message.setText("Please Wait")
             wait_dialog.show()
-            applyOrWithDrawFromBid(false, position, bidValue.text.toString())
+            when (categorisedItems) {
+                true -> applyOrWithDrawFromBid(false, position, bidValue.text.toString(), categorisedList)
+                else -> applyOrWithDrawFromBid(false, position, bidValue.text.toString(), itemsList)
+            }
         }
         dialog.show()
     }
 
 
-    private fun applyOrWithDrawFromBid(withDraw: Boolean, position: Int, bidValue: String) {
+    private fun applyOrWithDrawFromBid(
+        withDraw: Boolean,
+        position: Int,
+        bidValue: String,
+        list: ArrayList<Items>
+    ) {
         //Setting user in applied users list
-        appliedUsersList.putAll(itemList[position].bidded_users!!)
-      //  Toast.makeText(activity,"Applied Users ${appliedUsersList.toString()}",Toast.LENGTH_LONG).show()
+
         val dbRef = FirebaseFirestore.getInstance()
         when (withDraw) {
             true -> {
                 appliedUsersList.remove(userId!!)
-                dbRef.collection("Items").document(itemList[position].itemId)
+                dbRef.collection("Items").document(list[position].itemId)
                     .update("bidded_users", appliedUsersList)
                     .addOnSuccessListener {
                         appliedUsersList.clear()
                         wait_dialog.dismiss()
                         Toast.makeText(activity!!, "Success!", Toast.LENGTH_SHORT).show()
+                        adapter.notifyItemChanged(position)
                     }
                     .addOnFailureListener {
                         wait_dialog.dismiss()
                         Toast.makeText(activity!!, it.message, Toast.LENGTH_LONG).show()
                     }
-
             }
             false -> {
 //                if(appliedUsersList.size.equals(1) && appliedUsersList[0].equals("")) {
 //                    val dbRef = FirebaseFirestore.getInstance()
 //                    appliedUsersList[0]= userId!!
-//                    dbRef.collection("Items").document(itemList[position].itemId).
+//                    dbRef.collection("Items").document(categorisedList[position].itemId).
 //                        update("appliedUsers",appliedUsersList)
 //                } else {
                 appliedUsersList.put(userId!!, value)
-                if(bidValue.toInt() > maxVal) {
-                    dbRef.collection("Items").document(itemList[position].itemId)
+                if (bidValue.toInt() > maxVal) {
+                    dbRef.collection("Items").document(list[position].itemId)
                         .update("max_bid_amount", bidValue)
                 }
-                dbRef.collection("Items").document(itemList[position].itemId).update("bidded_users", appliedUsersList)
+                dbRef.collection("Items").document(list[position].itemId)
+                    .update("bidded_users", appliedUsersList)
                     .addOnSuccessListener {
                         appliedUsersList.clear()
                         wait_dialog.dismiss()
                         dialog.dismiss()
                         Toast.makeText(activity!!, "Success!", Toast.LENGTH_SHORT).show()
+                        adapter.notifyItemChanged(position)
                     }
                     .addOnFailureListener {
                         wait_dialog.dismiss()
@@ -180,26 +249,41 @@ class bidder_HomeFragment : Fragment() {
                     when (dc.type) {
                         DocumentChange.Type.ADDED -> {
                             val item = dc.document.toObject(Items::class.java)
-                            itemList.add(item)
+                            itemsList.add(item)
+                            Log.d("Item", item.toString())
                             adapter.notifyDataSetChanged()
-
                         }
                         DocumentChange.Type.MODIFIED -> {
                             val item = dc.document.toObject(Items::class.java)
-                            itemList.forEachIndexed { index, items ->
+                            itemsList.forEachIndexed { index, items ->
                                 if (items.itemId.equals(item.itemId)) {
-                                    //itemList.add(index, item)
-                                    itemList[index] = item
+                                    //categorisedList.add(index, item)
+                                    itemsList[index] = item
                                     adapter.notifyItemChanged(index)
                                 }
                             }
+                            if (categorisedItems) {
+                                categorisedList.forEachIndexed { indexs, catItem ->
+                                    if (catItem.itemId.equals(item.itemId)) {
+                                        categorisedList[indexs] = item
+                                        adapter.notifyDataSetChanged()
+                                    }
+                                }
+                            }
                         }
+
                         DocumentChange.Type.REMOVED -> {
                             for (doc in snapshots.documents) {
                                 val item = doc.toObject(Items::class.java)
-                                itemList.forEachIndexed { index, items ->
+                                itemsList.forEachIndexed { index, items ->
                                     if (items.itemId.equals(item?.itemId)) {
-                                        itemList.removeAt(index)
+                                        itemsList.removeAt(index)
+                                        adapter.notifyItemRemoved(index)
+                                    }
+                                }
+                                if (categorisedItems) {
+                                    categorisedList.forEachIndexed { index, items ->
+                                        categorisedList.removeAt(index)
                                         adapter.notifyItemRemoved(index)
                                     }
                                 }
