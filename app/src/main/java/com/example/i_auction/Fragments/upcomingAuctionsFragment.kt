2f8a9de389a3397dc5a6db06e0b391ecfb948b59
Adeel984 <43789830+Adeel984@users.noreply.github.com>
2019-05-22 18:11:04 +0500

    New addition of Upcoming auction fragments

--- app/src/main/java/com/example/i_auction/Fragments/upcomingAuctionsFragment.kt
+++ app/src/main/java/com/example/i_auction/Fragments/upcomingAuctionsFragment.kt
@@ -2,12 +2,21 @@ package com.example.i_auction.Fragments
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.i_auction.Adapters.ItemsRVAdapter
+import com.example.i_auction.Models.Items
 
 import com.example.i_auction.R
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,13 +29,71 @@ private const val ARG_PARAM2 = "param2"
  */
 class upcomingAuctionsFragment : Fragment() {
 
+    private lateinit var  adapter:ItemsRVAdapter
+    private val itemsList = ArrayList<Items>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upcoming_auctions, container, false)
+        val view = inflater.inflate(R.layout.fragment_upcoming_auctions, container, false)
+        val recyler = view. findViewById<RecyclerView>(R.id.upcoming_auction_Rv)
+        recyler.layoutManager = LinearLayoutManager(activity!!)
+        val auctionerClicked: (views: View, position: Int) -> Unit = { views, position ->
+            when (views.id) {
+
+            }
+        }
+        adapter = ItemsRVAdapter(activity!!,itemsList,auctionerClicked)
+        recyler.adapter = adapter
+        getAllItems()
+    return view
     }
 
+    private fun getAllItems() {
+        val dbRef = FirebaseFirestore.getInstance()
+        dbRef.collection("Items")
+            .whereEqualTo("soldOut", false)
+            .whereEqualTo("upcoming",true)
+            .whereEqualTo("withDraw",false)
+            .addSnapshotListener(EventListener<QuerySnapshot> { snapshots, e ->
+                if (e != null) {
+                    Log.d("Nodata", "No data")
+                    return@EventListener
+                }
+                for (dc in snapshots!!.documentChanges) {
+                    when (dc.type) {
+                        DocumentChange.Type.ADDED -> {
+                            val item = dc.document.toObject(Items::class.java)
+                            itemsList.add(item)
+                            Log.d("Item", item.toString())
+                            adapter.notifyDataSetChanged()
+                        }
+                        DocumentChange.Type.MODIFIED -> {
+                            val item = dc.document.toObject(Items::class.java)
+                            itemsList.forEachIndexed { index, items ->
+                                if (items.itemId.equals(item.itemId)) {
+                                    //categorisedList.add(index, item)
+                                    itemsList[index] = item
+                                    adapter.notifyItemChanged(index)
+                                }
+                            }
+                        }
 
+                        DocumentChange.Type.REMOVED -> {
+                            for (doc in snapshots.documents) {
+                                val item = doc.toObject(Items::class.java)
+                                itemsList.forEachIndexed { index, items ->
+                                    if (items.itemId.equals(item?.itemId)) {
+                                        itemsList.removeAt(index)
+                                        adapter.notifyItemRemoved(index)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+    }
 }
