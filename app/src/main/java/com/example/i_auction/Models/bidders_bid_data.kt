c6708abedaf4d2c58d9c416a998c82b008a045e5
Adeel984 <43789830+Adeel984@users.noreply.github.com>
2019-06-13 21:32:37 +0500

    added new model class

--- app/src/main/java/com/example/i_auction/Models/bidders_bid_data.kt
+++ app/src/main/java/com/example/i_auction/Models/bidders_bid_data.kt
@@ -1,4 +1,11 @@
 package com.example.i_auction.Models
 
-class bidders_bid_data {
+class bidders_bid_data (
+    val bidder_id:String ="",
+    val assurance:String ="",
+    val bidAmount:Int =0,
+    val itemId:String ="",
+    var accepted_bid:Boolean = false
+) {
+
 }
